0a24ce5b40d917813f1587a21c935d0afbaa1d3e
My Quyen <thaikhoamyquyen@gmail.com>
2019-08-01 11:48:45 +0700

    Basics features

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let tweets = [];
+let tweets = [{ body: "Hello" }];
+
+function renderSingleTweet(tweet) {
+  return `${tweet.body}`;
+}
+
+function renderTweets() {
+  let tweetsList = tweets.map(renderSingleTweet);
+  document.getElementById("tweets").innerHTML = tweetsList;
+}
+
+function createTweet() {
+  let tweet = {};
+  let tweetContent = document.getElementById("tweetInput").value;
+  tweet.body = tweetContent;
+  tweets.push(tweet);
+  renderTweets();
+}
+renderTweets();
