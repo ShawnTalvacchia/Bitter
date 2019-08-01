3ba294cce53cf3175c3a0cb83fde1af1c3ee09a4
Shawn Talvacchia <stalvacchia@gmail.com>
2019-08-01 13:17:26 +0700

    Added notes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let tweets = [{ body: "Hello" }];
 
+//Create Template for Tweet
 function renderSingleTweet(tweet) {
   return `${tweet.body}`;
 }
-
+//Take tweet array, apply template, put in HTML
 function renderTweets() {
   let tweetsList = tweets.map(renderSingleTweet);
   document.getElementById("tweets").innerHTML = tweetsList;
 }
 
+//Take input from user
 function createTweet() {
   let tweet = {};
   let tweetContent = document.getElementById("tweetInput").value;
