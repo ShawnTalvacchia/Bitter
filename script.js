026657da20ea403b1464b04bc64ac556c26ce952
My Quyen <thaikhoamyquyen@gmail.com>
2019-08-01 15:13:39 +0700

    Add features

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
-let tweets = [{ body: "Hello" }];
+let tweets = [];
+let inputField = document.getElementById("tweetInput");
 
-function renderSingleTweet(tweet) {
-  return `${tweet.body}`;
+// Create new tweet object
+function newTweet(body, userName, isLiked = false) {
+  return {
+    body,
+    isLiked,
+    userName
+  };
 }
 
+tweets.push(newTweet("hello", (userName = "Mai")));
+tweets.push(newTweet("Twitter", (userName = "Quyen")));
+tweets.push(newTweet("Mai", (userName = "Shawn")));
+tweets.push(newTweet("Hello again", (userName = "Parsa")));
+
+//Create template for tweet
+function renderSingleTweet(tweet, index) {
+  return `
+  <li>
+    <h2> ${tweet.userName} </h2> 
+    <p> ${tweet.body} </p>
+    <button class="btn btn-sm btn-outline-success complete" onclick=toggle(${index})>${
+    tweet.isLiked ? "Unlike" : "Like"
+  }</button> ${tweet.isLiked}
+</li>
+    `;
+}
+
+//Take tweet array, apply template to each element, put in HTML
 function renderTweets() {
   let tweetsList = tweets.map(renderSingleTweet);
-  document.getElementById("tweets").innerHTML = tweetsList;
+  document.getElementById("tweets").innerHTML = tweetsList.join("");
 }
 
+//Take input from user and create new tweet
 function createTweet() {
-  let tweet = {};
-  let tweetContent = document.getElementById("tweetInput").value;
-  tweet.body = tweetContent;
+  let tweetContent = inputField.value;
+  let tweet = newTweet(tweetContent, "Quyen");
   tweets.push(tweet);
   renderTweets();
+  inputField.value = "";
+  updateLength();
 }
+
+function toggle(index) {
+  tweets[index].isLiked = !tweets[index].isLiked;
+  renderTweets();
+}
+
+function updateLength() {
+  let tweetString = inputField.value.length;
+  document.getElementById("tweetLength").innerHTML = 140 - tweetString;
+}
+
+document.getElementById("tweetInput").focus();
+inputField.addEventListener("keyup", updateLength());
+
 renderTweets();
